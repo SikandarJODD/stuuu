09fc193e96c05188ffb5558391b8cc3c064a8f91
SikandarJODD <vu1f2021072@pvppcoe.ac.in>
2023-02-17 05:07:31 +0530

    Added icon

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,14 @@
 <script>
 	import Card from '$lib/Card.svelte';
 	import './style.css';
+	import Icon from './icon.jpg';
 </script>
 
+<svelte:head>
+	<title>Stuu</title>
+	<link rel="shortcut icon" href={Icon} type="image/x-icon" />
+	<meta name="description" content="Website by Sikandar.S.Bhide , Made in Love " />
+</svelte:head>
 <div class="container">
 	<!-- <h1>Welcome to Fili - Phili</h1> -->
 	<Card />
